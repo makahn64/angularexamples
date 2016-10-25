008101ac295136ebe79bf2f14ceb0f2a0ee8c9ce
Mitch Kahn <mitch@ourglass.io>
2016-10-25 16:40:21 -0700

    Fixing READMEs for Hello Angular.

--- HelloAngular/helloAngular.js
+++ HelloAngular/helloAngular.js
@@ -5,6 +5,16 @@
 //"app" lives in the global JS namespace.
 var app = angular.module("helloApp", []);
 
+app.controller( "divController", function ( $scope, $log ) {
+
+   $log.debug("loading divController");
+
+    $scope.clear = function(){
+        $log.debug("Clear in the child scope");
+        $scope.$parent.clear();
+    }
+
+});
 
 
 app.controller("mainController", function ($scope) {
@@ -12,11 +22,13 @@ app.controller("mainController", function ($scope) {
     //I am hidden from the global namespace and not visible to the DOM
     var localVar = "dude";
 
+    $scope.localVar = localVar + " hey now!";
+
     //I am also hidden from the global namespace, but since I am "$scope", I am visible to the DOM
     $scope.name = "John Smith Jr.";
 
     // Some more variables, also visible in the DOM.
-    $scope.shoppingList = [ 'Eggs', 'Bread', 'Butter', 'Lettuce' ];
+    $scope.shoppingList = [ 'Eggs', 'Bread', 'Butter', 'Lettuce', 'Beer' ];
 
 
     var myObj = {
@@ -24,7 +36,9 @@ app.controller("mainController", function ($scope) {
         topVal: 100,
         nestObj: {
             stackOfCups: 20,
-            sayHi: function(){ console.log("hi!")}
+            sayHi: function(){ 
+                console.log("hi!")
+                }
 
         }
 
@@ -51,5 +65,6 @@ app.controller("mainController", function ($scope) {
     $scope.removeItem = function(index){
         // Delete an item from the shopping list
     }
+
 
 });
